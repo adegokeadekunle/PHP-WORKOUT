7eef08a3989d96280ce82a7bac77fd2ec4259f74
kunlay <ade@naubank.com.ec>
2023-04-14 17:09:19 +0100

    added some details

--- 07_arrays_functions.php
+++ 07_arrays_functions.php
@@ -31,4 +31,41 @@ $arr1 =[1,2,3,4];
 $arr2 =[5,6,7,8];
 
 $arr3 = array_merge($arr1,$arr2);
+$arr4 = [...$arr1,...$arr3];  // you can also use the spread operator to add the arrays you need to add up
+
 print_r($arr3);
+
+//COMBINE ARRAYS
+$color = ['yellow','green','red'];
+$edibles = ['orange','vegetables','apple'];
+$combined_color_edible = array_combine($color,$edibles); // this will pair the both arrays by using the first array as key and second array as value
+print_r($combined_color_edible);
+
+$keys = array_keys($combined_color_edible); //these will return the keys in the combind arrays
+print_r($keys);
+
+$values = array_values($combined_color_edible);// this will return the values in the combined array
+print_r($values);
+
+$flip = array_flip($combined_color_edible); //this will flip the keys as value and value as key i.e color take the position edible and vice-versa
+print_r($flip);
+
+//GETTING A RANGE
+$numbers = range(1,10); // this will return numbers from 1 to 10
+print_r($numbers);
+
+//USING MAP FUNCTION
+$newNumbers = range(1,20);
+$map_to_string = array_map(function($newNumbers){
+    // return "Number ${newNumbers}";
+    return 'Number'.' '.$newNumbers;
+},$newNumbers);
+print_r($map_to_string); 
+
+//FILTERING ARRAYS
+$less_than = array_filter($newNumbers, fn($num) => $num <=5 );
+print_r($less_than);
+
+//USING REDUCE FUNCTION 
+$sum = array_reduce($newNumbers , fn($carry,$num) => $carry + $num); // the carry holds an element of 0 so this add up numbers from 0 position
+var_dump($sum);
