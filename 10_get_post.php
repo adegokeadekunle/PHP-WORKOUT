10d1b7b15d5296de8748ef2a5b3303ac1393b344
kunlay <adegokeadekunle2@gmail.com>
2023-04-14 20:33:43 +0100

    added new commit

--- 10_get_post.php
+++ 10_get_post.php
@@ -6,7 +6,11 @@ We can pass data through urls and forms using the $_Get and $_Post superglobals.
 // echo $_GET['name'];
 // echo $_GET['age'];
 
-// for security and not to expose data , it is a good practise to use 
+// for security and not to expose data , it is a good practise to use $_POST so the info is not displayed
+if(isset($_POST['submit'])){ 
+ echo $_POST['name'];
+ echo $_POST['age'];
+}
 ?>
 
 <a href="<?php echo $_SERVER['PHP_SELF'];?>?name=kunle&age=20">Click</a>
